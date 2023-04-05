8b1b5bd4153a3f4066fb9ed41d2727b527ece6f4
tung175 <duongkq3@gmail.com>
2023-04-06 00:02:06 +0700

    dduyx duy bo

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Chào Bé ! 🖐🖐🖐',
+    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
+    btnYes: 'em khum nhớ 👉👈🥺',
+    btnNo: '💔 Em cóa 😏 💔 ',
+    question:'Cố nhớ đi không là anh dỗi 😒',
+    reasonPlaceholder: 'Trả lời nào Bé iu',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Hôm nay là ngày 6/4, còn ngày gì thì vẫn khum nhớ',
+    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://tung175.github.io/HBBD/'
+}
